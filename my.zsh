f855080038ddc05af2655cfcd190de62b138ad5d
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2022-03-14 16:45:06 +0100

    add custom rc my.zsh

---
+++ my.zsh
@@ -0,0 +1,14 @@
+alias tm="tmux new -s $1"
+alias tma="tmux attach -t $1"
+alias sv="mosh pj@116.203.104.224"
+# alias nvim="lvim"
+alias lg="lazygit"
+alias pubip="curl ifconfig.co"
+
+# Projects
+alias nv1="cd ~/Documents/Projects/rcap.tv/rcap && nvim"
+alias nvc="cd ~/.config/nvim && nvim"
+
+# Exports
+export GO111MODULE=on
+export XDG_CONFIG_HOME="$HOME/.config"
