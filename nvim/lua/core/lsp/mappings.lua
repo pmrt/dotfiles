c03ae3a7409a183639062876369b2806aa691312
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2022-04-05 17:41:24 +0200

    lsp: add mappings

--- nvim/lua/core/lsp/mappings.lua
+++ nvim/lua/core/lsp/mappings.lua
@@ -5,24 +5,20 @@ function M.on_attach(client, bufnr)
 
   -- ref: ':help vim.lsp.*'
   map(bufnr, 'n', 'gd', '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>')
+  map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
   map(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-  map(bufnr, 'n', 'gi', '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>')
-  map(bufnr, 'n', 'gt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>')
-  map(bufnr, 'n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
-  map(bufnr, 'n', 'gn', '<cmd>lua require("cosmic-ui").rename()<cr>')
+  map(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+  map(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<cr>')
 
   -- diagnostics
-  map(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-  map(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  map(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>')
+  map(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<cr>')
+  map(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<cr>')
+  map(bufnr, 'n', 'gl', '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>')
   map(bufnr, 'n', '<leader>ge', '<cmd>Telescope diagnostics bufnr=0<cr>')
 
   -- hover
   map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-
-  -- code actions
-  map(bufnr, 'n', '<leader>ga', '<cmd>lua require("cosmic-ui").code_actions()<cr>')
-  map(bufnr, 'v', '<leader>ga', '<cmd>lua require("cosmic-ui").range_code_actions()<cr>')
 
   -- formatting
   map(bufnr, 'n', '<leader>gf', '<cmd>lua vim.lsp.buf.formatting()<cr>')
@@ -30,14 +26,6 @@ function M.on_attach(client, bufnr)
 
   -- lsp workspace
   map(bufnr, 'n', '<leader>wd', '<cmd>Telescope diagnostics<cr>')
-  map(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>')
-  map(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>')
-  map(
-    bufnr,
-    'n',
-    '<leader>wl',
-    '<cmd>lua require("cosmic.utils.logger"):log(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>'
-  )
 
   if client.name == 'tsserver' then
     -- typescript helpers
