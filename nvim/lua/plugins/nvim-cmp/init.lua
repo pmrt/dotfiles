468da3771c287c3f5a8d1d74f48e13da1dbb6df6
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2022-06-08 14:14:01 +0200

    indent

--- nvim/lua/plugins/nvim-cmp/init.lua
+++ nvim/lua/plugins/nvim-cmp/init.lua
@@ -108,13 +108,13 @@ plugin.setup.cmdline('/', {
     { name = 'buffer' },
   }
 })
-plugin.setup.cmdline(':', {
-  sources = plugin.config.sources({
-  { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-    })
-})
+-- plugin.setup.cmdline(':', {
+--   sources = plugin.config.sources({
+--   { name = 'path' }
+--   }, {
+--     { name = 'cmdline' }
+--     })
+-- })
 
 vim.cmd([[
   augroup DisableCmpOnTelescopeGroup
